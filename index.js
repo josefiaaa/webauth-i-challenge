9516c04b2e6f3e54ad7d74c39e72a5ba21a17ec0
“Josefia <josefia.magagnoli@gmail.com>
2019-05-06 19:13:19 -0400

    Find all users if logged in

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const cors = require('cors')
 const morgan = require('morgan');
 const bcrypt = require('bcryptjs');
 
-const Users = require('./users/users-model')
-
+const Users = require('./users/users-model');
+const protected = require('./auth/protected-middleware.js');
 
 const server = express();
 
@@ -39,18 +39,26 @@ server.post('/api/login', (req, res) => {
   let { username, password } = req.body;
 
   Users.findBy({ username })
-      .first()
-      .then(user => {
+        .first()
+        .then(user => {
           if (user && bcrypt.compareSync(password, user.password)) {
               res.status(200).json({ message: `Welcome ${user.username}!` })
             } else {
-              res.status(401).json({ message: 'You shall not pass' })
+              res.status(401).json({ message: 'You shall not pass!' })
             }
-      })
-      .catch(err => {
+        })
+        .catch(err => {
           res.status(500).json(err)
         })
 })
+
+server.get('/api/users', protected, (req, res) => {
+    Users.find()
+        .then(users => {
+            res.json(users);
+        })
+        .catch(err => res.send(err));
+});
 
 
 const port = process.env.PORT || 5000;
